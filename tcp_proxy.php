2f65f28e302121eb0a11e473245893e235432508
wpjscc <76907477+wpjscc@users.noreply.github.com>
2023-08-18 08:46:21 +0800

    Update tcp_proxy.php

--- tcp_proxy.php
+++ tcp_proxy.php
@@ -11,7 +11,13 @@ $socket->on('connection', function (React\Socket\ConnectionInterface $proxyConne
         $buffer .= $data;
     });
 
-    (new React\Socket\Connector(array('timeout' => 3.0)))
+    (new React\Socket\Connector(array(
+        'timeout' => 3.0,
+        // 'tcp' => new Clue\React\HttpProxy\ProxyConnector('192.168.43.1:8234'), //可以做个跳板(http proxy)
+        // 'tcp' => new Clue\React\Socks\Client('192.168.43.1:8235'), // 可以做个跳板(socket proxy),
+        // 'tcp' => new Clue\React\SshProxy\SshProcessConnector('user@ip'), //可以做个跳板(ssh proxy)
+        // 'dns' => false,
+    )))
     ->connect("tcp://".getParam("--dest-host").":".getParam("--dest-port"))
     ->then(function (React\Socket\ConnectionInterface $connection) use ($proxyConnection, $fn, &$buffer)  {
 
